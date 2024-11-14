839038d16d76fc0277283f7c86f7f2e52bbe3394
Rachele Barberis <rachele.barberis.12@gmail.com>
2024-11-14 09:25:30 +0100

    inizio

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//calendario dinamico che cambia giorno in base a che giorno della settimana siamo
+//cliccando su una cella del calendario sarà possibile vedere gli appuntamenti
+//verrà fuori un promemoria per dirci che abbiamo un appuntamento
+/* progetto: 1. applicazione DINAMICA,che genera un calendario che si aggiorna
+2. per ogni giorno è possibile creare voci, e quando un giorno ha delle cose salvate, si modifica con un segnaposto che indica che per quel giorno abbiamo delle voci*/
+
+
